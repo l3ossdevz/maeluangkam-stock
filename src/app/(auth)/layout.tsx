e0d3f147e16c79c49735a505989633c6789f1f53
Kritsada Tariya <mr.kritsada.t@gmail.com>
2023-11-29 18:46:15 +0700

    :art: Setup structure project

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+type Props = { children: React.ReactNode }
+
+export default function AuthLayout({ children }: Props) {
+  return <div>{children}</div>
+}
---
+++ src/app/(routes)/stock/add/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+type Props = {}
+
+export default function AddPage({}: Props) {
+  return <div>AddPage</div>
+}
